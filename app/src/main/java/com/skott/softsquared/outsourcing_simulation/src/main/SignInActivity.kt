33f481bc28b637e9a3acfb54cc5f9eb1b2145bc7
DESKTOP-5B9VU1L\skine <skine134@naver.com>
2021-05-23 00:34:20 +0900

    sign_in margin, padding 처리

---
+++ app/src/main/java/com/skott/softsquared/outsourcing_simulation/src/main/SignInActivity.kt
@@ -0,0 +1,7 @@
+package com.skott.softsquared.outsourcing_simulation.src.main
+
+import com.skott.config.config.BaseActivity
+import com.skott.softsquared.outsourcing_simulation.databinding.SignInLayoutBinding
+
+class SignInActivity :BaseActivity<SignInLayoutBinding>(SignInLayoutBinding::inflate){
+}
