8c1639d6c0e61a1e6d44446d1de2b6fa72cef755
DESKTOP-5B9VU1L\skine <skine134@naver.com>
2021-05-27 03:47:28 +0900

    현재 위치로 찾기 레이아웃 구성.

---
+++ app/src/main/java/com/skott/softsquared/outsourcing_simulation/src/util/custom_views/SearchView.kt
@@ -0,0 +1,20 @@
+package com.skott.softsquared.outsourcing_simulation.src.util.custom_views
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.skott.softsquared.outsourcing_simulation.databinding.SearchViewBinding
+
+class SearchView(context: Context, attrs:AttributeSet, defStyleAttr:Int=0) :ConstraintLayout(context,attrs,defStyleAttr){
+    private lateinit var binding:SearchViewBinding
+    init{
+        init()
+    }
+    fun init()
+    {
+        val inflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        binding = SearchViewBinding.inflate(inflater,this,false)
+        addView(binding.root)
+    }
+}
