59cdaf68130106ee4c6017c2f88c8a9ee35b6698
DESKTOP-5B9VU1L\skine <skine134@naver.com>
2021-05-31 02:17:17 +0900

    동네 인증 뷰가 리사이클러뷰에 들어갈 수 있도록 뷰홀더 변경.

--- app/src/main/java/com/skott/softsquared/outsourcing_simulation/src/main/home_product_list/ProductRecyclerViewHolder.kt
+++ app/src/main/java/com/skott/softsquared/outsourcing_simulation/src/main/home_product_list/ProductRecyclerViewHolder.kt
@@ -5,35 +5,59 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.RecyclerView
+import androidx.viewbinding.ViewBinding
 import com.skott.softsquared.outsourcing_simulation.R
+import com.skott.softsquared.outsourcing_simulation.databinding.PleaseTownAuthFragmentBinding
 import com.skott.softsquared.outsourcing_simulation.databinding.ProductItemAdapterBinding
+import com.skott.softsquared.outsourcing_simulation.databinding.ProductListFragmentBinding
 import com.skott.softsquared.outsourcing_simulation.src.main.home_product_list.model.ProductListResponse
 import com.skott.softsquared.outsourcing_simulation.src.util.custom_views.IconCounterView
 import com.skott.softsquared.outsourcing_simulation.src.util.lib.getRoundedAllCornerBitmap
 
-class ProductRecyclerViewHolder(val context: Context, val item:ProductItemAdapterBinding) :RecyclerView.ViewHolder(item.root){
-    private val name:TextView = item.productNameTextView
-    private val town:TextView = item.sellerTownTextView
-    private val image: ImageView = item.productImageView
-    private val price:TextView = item.productPriceTextView
-    private val favorite:IconCounterView = item.favoriteCount
-    private val chat:IconCounterView = item.chatCount
-    private val multiChat:IconCounterView = item.multiChatCount
-    private val time:TextView = item.productUploadTimeTextView
-    private val pullUp:TextView = item.pullUpTextView
-    var status : String="ONSALE"
-    fun bind(productListResponse: ProductListResponse)
-    {
-        name.text=productListResponse.title
-        town.text=productListResponse.dong
+class ProductRecyclerViewHolder(val context: Context, val item: ViewBinding) :
+    RecyclerView.ViewHolder(item.root) {
+    private lateinit var name: TextView
+    private lateinit var town: TextView
+    private lateinit var image: ImageView
+    private lateinit var price: TextView
+    private lateinit var favorite: IconCounterView
+    private lateinit var chat: IconCounterView
+    private lateinit var multiChat: IconCounterView
+    private lateinit var time: TextView
+    private lateinit var pullUp: TextView
+    private lateinit var noticeView: ConstraintLayout
+    var status: String = "ONSALE"
+
+    init {
+        if (item is ProductItemAdapterBinding) {
+            name = item.productNameTextView
+            town = item.sellerTownTextView
+            image = item.productImageView
+            price = item.productPriceTextView
+            favorite = item.favoriteCount
+            chat = item.chatCount
+            multiChat = item.multiChatCount
+            time = item.productUploadTimeTextView
+            pullUp = item.pullUpTextView
+        } else {
+            noticeView = (item as PleaseTownAuthFragmentBinding).root
+        }
+    }
+
+    fun bind(productListResponse: ProductListResponse?) {
+        if (productListResponse == null)
+            return
+        name.text = productListResponse.title
+        town.text = productListResponse.dong
         val imageString = productListResponse.pictureUrl.toString()
-        if(!imageString.equals("null"))
-            getRoundedAllCornerBitmap(context,imageString,20,image)
-        price.text = context.getString(R.string.product_price).replace("price",productListResponse.price)
+        if (!imageString.equals("null"))
+            getRoundedAllCornerBitmap(context, imageString, 20, image)
+        price.text =
+            context.getString(R.string.product_price).replace("price", productListResponse.price)
         favorite.setCount(productListResponse.numOfLikes)
         chat.setCount(productListResponse.numOfChats)
-        time.text=productListResponse.passedTime
-        pullUp.text = if(productListResponse.isOnTop.equals("NO")) "" else "끌올"
+        time.text = productListResponse.passedTime
+        pullUp.text = if (productListResponse.isOnTop.equals("NO")) "" else "끌올"
         status = productListResponse.status
     }
 }
